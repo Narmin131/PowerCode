339b4b7518074881c1c0b17d96ffc360b23fe9b9
Narmin <mnermin064@gmail.com>
2024-02-27 17:38:25 +0400

    Array methods

---
+++ lesson2/assets/js/index.js
@@ -0,0 +1,80 @@
+// map , filter, foreach, find, every, some
+
+const characters = [
+    {
+        name: 'Luke Skywalker',
+        height: 172,
+        mass: 77,
+        eye_color: 'blue',
+        gender: 'male',
+    },
+    {
+        name: 'Darth Vader',
+        height: 202,
+        mass: 136,
+        eye_color: 'yellow',
+        gender: 'male',
+    },
+    {
+        name: 'Leia Organa',
+        height: 150,
+        mass: 49,
+        eye_color: 'brown',
+        gender: 'female',
+    },
+    {
+        name: 'Anakin Skywalker',
+        height: 188,
+        mass: 84,
+        eye_color: 'blue',
+        gender: 'male',
+    },
+];
+
+// 1
+let mapMethod = characters.map((item)=>item.height)
+console.log(mapMethod);
+
+// 2
+let sortMethod = characters.sort((a,b)=>a.mass - b.mass)
+console.table(sortMethod);
+
+// 3
+
+let everyMethod = characters.every((item)=>item.gender == 'male')
+console.log(everyMethod);
+
+
+const people = [
+    { firstName: 'Sam', lastName: 'Hughes', DOB: '07/07/1978', department: 'Development', salary: '45000' },
+    { firstName: 'Terri', lastName: 'Bishop', DOB: '02/04/1989', department: 'Development', salary: '35000' },
+    { firstName: 'Jar', lastName: 'Burke', DOB: '11/01/1985', department: 'Marketing', salary: '38000' },
+    { firstName: 'Julio', lastName: 'Miller', DOB: '12/07/1991', department: 'Sales', salary: '40000' },
+    { firstName: 'Chester', lastName: 'Flores', DOB: '03/15/1988', department: 'Development', salary: '41000' },
+    { firstName: 'Madison', lastName: 'Marshall', DOB: '09/22/1980', department: 'Sales', salary: '32000' },
+    { firstName: 'Ava', lastName: 'Pena', DOB: '11/02/1986', department: 'Development', salary: '38000' },
+    { firstName: 'Gabriella', lastName: 'Steward', DOB: '08/26/1994', department: 'Marketing', salary: '46000' },
+    { firstName: 'Charles', lastName: 'Campbell', DOB: '09/04/1977', department: 'Sales', salary: '42000' },
+    { firstName: 'Tiffany', lastName: 'Lambert', DOB: '05/11/1990', department: 'Development', salary: '34000' },
+    { firstName: 'Antonio', lastName: 'Gonzalez', DOB: '03/24/1985', department: 'Office Management', salary: '49000' },
+    { firstName: 'Aaron', lastName: 'Garrett', DOB: '09/04/1985', department: 'Development', salary: '39000' },
+];
+// 1. Konsola userlerin yalniz firstName ve lastName cixardin
+// 2. Userleri yasca kicikden boyuye duzun
+// 3. Yasi 40 dan boyuk olan userleri ekrana yazdirin
+// 4. Yalniz marketing departamentinin userlarini ekrana yazdirin
+// 5. Madison Marhsallin yasini tapin
+
+// map
+// sort
+// filter
+// filter
+// find
+
+let date = new Date().getSeconds()
+console.log(date);
+
+let a = new Date('09/04/1985')
+console.log(a);
+
+map((item)=>new Date().getFullYear() - new Date(item.DOB).getFullYear())
